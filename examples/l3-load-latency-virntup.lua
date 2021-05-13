d616ddc567d64b000ec0d167743b0b6a12323e80
Matthias Linhuber <matthias.linhuber@tum.de>
2021-05-13 13:27:32 +0200

    Fix MAC/IP CLI config implementation

--- examples/l3-load-latency-virntup.lua
+++ examples/l3-load-latency-virntup.lua
@@ -10,36 +10,23 @@ local arp    = require "proto.arp"
 local log    = require "log"
 
 -- set addresses here
-local DST_MAC		= "" -- resolved via ARP on GW_IP or DST_IP, can be overriden with a string here
-local SRC_IP_BASE	= "10.0.0.10" -- actual address will be SRC_IP_BASE + random(0, flows)
-local DST_IP		= "10.1.0.10"
+local SRC_IP_BASE	= "10.42.1.1" -- actual address will be SRC_IP_BASE + random(0, flows)
 local SRC_PORT		= 1234
 local DST_PORT		= 319
-
--- answer ARP requests for this IP on the rx port
--- change this if benchmarking something like a NAT device
-local RX_IP		= DST_IP
--- used to resolve DST_MAC
-local GW_IP		= DST_IP
--- used as source IP to resolve GW_IP to DST_MAC
-local ARP_IP	= SRC_IP_BASE
 
 function configure(parser)
 	parser:description("Generates UDP traffic and measure latencies. Edit the source to modify constants like IPs.")
 	parser:argument("txDev", "Device to transmit from."):convert(tonumber)
 	parser:argument("rxDev", "Device to receive from."):convert(tonumber)
+	parser:argument("destIP", "Destination IP")
+	parser:argument("destMAC", "Destination MAC address")
 	parser:option("-r --rate", "Transmit rate in Mbit/s."):default(10000):convert(tonumber)
 	parser:option("-f --flows", "Number of flows (randomized source IP)."):default(4):convert(tonumber)
 	parser:option("-s --size", "Packet size."):default(60):convert(tonumber)
 	parser:option("--file", "Filename of the latency histogram."):default("histogram.csv")
-	parser:option("--destIP", "Destination IP"):default("10.42.3.1")
-	parser:option("--destMAC", "Destination MAC address"):default("08:00:00:00:3:1")
 end
 
 function master(args)
-        -- Make Destination ip and MAC address configurable form CLI
-        DST_IP = args.destIP
-        DST_MAC = args.destMAC
 
 	txDev = device.config{port = args.txDev, rxQueues = 3, txQueues = 3}
 	rxDev = device.config{port = args.rxDev, rxQueues = 3, txQueues = 3}
@@ -49,32 +36,26 @@ function master(args)
 	if args.rate > 0 then
 		txDev:getTxQueue(0):setRate(args.rate - (args.size + 4) * 8 / 1000)
 	end
-	mg.startTask("loadSlave", txDev:getTxQueue(0), rxDev, args.size, args.flows)
-	mg.startTask("timerSlave", txDev:getTxQueue(1), rxDev:getRxQueue(1), args.size, args.flows, args.file)
-	arp.startArpTask{
-		-- run ARP on both ports
-		{ rxQueue = rxDev:getRxQueue(2), txQueue = rxDev:getTxQueue(2), ips = RX_IP },
-		-- we need an IP address to do ARP requests on this interface
-		{ rxQueue = txDev:getRxQueue(2), txQueue = txDev:getTxQueue(2), ips = ARP_IP }
-	}
+	mg.startTask("loadSlave", txDev:getTxQueue(0), rxDev, args.size, args.flows, args)
+	mg.startTask("timerSlave", txDev:getTxQueue(1), rxDev:getRxQueue(1), args.size, args.flows, args)
 	mg.waitForTasks()
 end
 
-local function fillUdpPacket(buf, len)
+local function fillUdpPacket(buf, len, args)
 	buf:getUdpPacket():fill{
 		ethSrc = queue,
-		ethDst = DST_MAC,
+		ethDst = args.destMAC,
 		ip4Src = SRC_IP,
-		ip4Dst = DST_IP,
+		ip4Dst = args.destIP,
 		udpSrc = SRC_PORT,
 		udpDst = DST_PORT,
 		pktLength = len
 	}
 end
 
-function loadSlave(queue, rxDev, size, flows)
+function loadSlave(queue, rxDev, size, flows, args)
 	local mempool = memory.createMemPool(function(buf)
-		fillUdpPacket(buf, size)
+		fillUdpPacket(buf, size, args)
 	end)
 	local bufs = mempool:bufArray()
 	local counter = 0
@@ -98,7 +79,7 @@ function loadSlave(queue, rxDev, size, flows)
 	rxCtr:finalize()
 end
 
-function timerSlave(txQueue, rxQueue, size, flows, histfile)
+function timerSlave(txQueue, rxQueue, size, flows, args)
 	if size < 84 then
 		log:warn("Packet size %d is smaller than minimum timestamp size 84. Timestamped packets will be larger than load packets.", size)
 		size = 84
@@ -111,7 +92,7 @@ function timerSlave(txQueue, rxQueue, size, flows, histfile)
 	local baseIP = parseIPAddress(SRC_IP_BASE)
 	while mg.running() do
 		hist:update(timestamper:measureLatency(size, function(buf)
-			fillUdpPacket(buf, size)
+			fillUdpPacket(buf, size, args)
 			local pkt = buf:getUdpPacket()
 			pkt.ip4.src:set(baseIP + counter)
 			counter = incAndWrap(counter, flows)
@@ -122,6 +103,6 @@ function timerSlave(txQueue, rxQueue, size, flows, histfile)
 	-- print the latency stats after all the other stuff
 	mg.sleepMillis(300)
 	hist:print()
-	hist:save(histfile)
+	hist:save(args.file)
 end
 
