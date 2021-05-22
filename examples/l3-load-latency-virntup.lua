0c0bccfa907704ab834a1c8a934f6cd2f40a773c
Matthias Linhuber <matthias.linhuber@tum.de>
2021-05-22 13:22:20 +0200

    Remove redundant rx/tx filename-prefix

--- examples/l3-load-latency-virntup.lua
+++ examples/l3-load-latency-virntup.lua
@@ -61,8 +61,8 @@ function loadSlave(queue, rxDev, size, flows, args)
 	end)
 	local bufs = mempool:bufArray()
 	local counter = 0
-        local txCtr = stats:newDevTxCounter(queue, "CSV", "tx-" .. args.txthroughputfile)
-        local rxCtr = stats:newDevRxCounter(rxDev, "CSV", "rx-" .. args.rxthroughputfile)
+        local txCtr = stats:newDevTxCounter(queue, "CSV", args.txthroughputfile)
+        local rxCtr = stats:newDevRxCounter(rxDev, "CSV", args.rxthroughputfile)
 	local baseIP = parseIPAddress(SRC_IP_BASE)
 	while mg.running() do
 		bufs:alloc(size)
