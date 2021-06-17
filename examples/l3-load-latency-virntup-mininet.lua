cfc9167d2ce3d7ee11a65281395fa0e214742d4b
Matthias Linhuber <matthias.linhuber@tum.de>
2021-06-17 14:16:45 +0200

    Add first version mininet measurement scirpt

---
+++ examples/l3-load-latency-virntup-mininet.lua
@@ -0,0 +1,112 @@
+local mg     = require "moongen"
+local memory = require "memory"
+local device = require "device"
+local ts     = require "timestamping"
+local filter = require "filter"
+local hist   = require "histogram"
+local stats  = require "stats"
+local timer  = require "timer"
+local arp    = require "proto.arp"
+local log    = require "log"
+local limiter = require "software-ratecontrol"
+
+
+-- set addresses here
+local SRC_IP_BASE	= "10.42.1.1" -- actual address will be SRC_IP_BASE + random(0, flows)
+local SRC_PORT		= 1234
+local DST_PORT		= 319
+
+function configure(parser)
+	parser:description("Generates UDP traffic and measure latencies. Edit the source to modify constants like IPs.")
+	parser:argument("txDev", "Device to transmit from."):convert(tonumber)
+	parser:argument("rxDev", "Device to receive from."):convert(tonumber)
+	parser:argument("destIP", "Destination IP")
+	parser:argument("destMAC", "Destination MAC address")
+	parser:option("-r --rate", "Transmit rate in kbit/s."):default(1000):convert(tonumber)
+	parser:option("-f --flows", "Number of flows (randomized source IP)."):default(4):convert(tonumber)
+	parser:option("-s --size", "Packet size."):default(60):convert(tonumber)
+	parser:option("--latencyfile", "Filename of the latency histogram."):default("latencyhistogram.csv")
+	parser:option("--rxthroughputfile", "Filename of the rx throughput csv."):default("rx_throughput.csv")
+	parser:option("--txthroughputfile", "Filename of the tx throughput csv."):default("tx_throughput.csv")
+end
+
+function master(args)
+
+	txDev = device.config{port = args.txDev, rxQueues = 3, txQueues = 3, disableOffloads = true}
+	-- txDev = device.config{port = args.txDev, rxQueues = 3, txQueues = 3}
+	rxDev = device.config{port = args.rxDev, rxQueues = 3, txQueues = 3}
+	device.waitForLinks()
+
+	local rateLimiter = limiter:new(txDev:getTxQueue(0),"cbr", 1 / args.rate * 1000000)  -- Supply delay in ns! 
+	mg.startTask("loadSlave", txDev:getTxQueue(0), rxDev, args.size, args.flows, args, rateLimiter)
+
+	-- mg.startTask("timerSlave", txDev:getTxQueue(1), rxDev:getRxQueue(1), args.size, args.flows, args)
+	mg.waitForTasks()
+end
+
+local function fillUdpPacket(buf, len, args)
+	buf:getUdpPacket():fill{
+		ethSrc = queue,
+		ethDst = args.destMAC,
+		ip4Src = SRC_IP,
+		ip4Dst = args.destIP,
+		udpSrc = SRC_PORT,
+		udpDst = DST_PORT,
+		pktLength = len
+	}
+end
+
+function loadSlave(queue, rxDev, size, flows, args, rateLimiter)
+	local mempool = memory.createMemPool(function(buf)
+		fillUdpPacket(buf, size, args)
+	end)
+	local bufs = mempool:bufArray()
+	local counter = 0
+        local txCtr = stats:newDevTxCounter(queue, "CSV", args.txthroughputfile)
+        local rxCtr = stats:newDevRxCounter(rxDev, "CSV", args.rxthroughputfile)
+	local baseIP = parseIPAddress(SRC_IP_BASE)
+	while mg.running() do
+		bufs:alloc(size)
+		for i, buf in ipairs(bufs) do
+			local pkt = buf:getUdpPacket()
+			pkt.ip4.src:set(baseIP + counter)
+			counter = incAndWrap(counter, flows)
+		end
+		-- UDP checksums are optional, so using just IPv4 checksums would be sufficient here
+		-- bufs:offloadUdpChecksums()
+		-- queue:send(bufs)
+		rateLimiter:send(bufs)
+		txCtr:update()
+		rxCtr:update()
+	end
+	txCtr:finalize()
+	rxCtr:finalize()
+end
+
+function timerSlave(txQueue, rxQueue, size, flows, args)
+	if size < 84 then
+		log:warn("Packet size %d is smaller than minimum timestamp size 84. Timestamped packets will be larger than load packets.", size)
+		size = 84
+	end
+	local timestamper = ts:newUdpTimestamper(txQueue, rxQueue)
+	local hist = hist:new()
+	mg.sleepMillis(1000) -- ensure that the load task is running
+	local counter = 0
+	local rateLimit = timer:new(0.001)
+	local baseIP = parseIPAddress(SRC_IP_BASE)
+	while mg.running() do
+		hist:update(timestamper:measureLatency(size, function(buf)
+			fillUdpPacket(buf, size, args)
+			local pkt = buf:getUdpPacket()
+			pkt.ip4.src:set(baseIP + counter)
+			counter = incAndWrap(counter, flows)
+		end))
+		rateLimit:wait()
+		rateLimit:reset()
+	end
+	-- print the latency stats after all the other stuff
+	mg.sleepMillis(300)
+	hist:print()
+	hist:save(args.latencyfile)
+end
+
